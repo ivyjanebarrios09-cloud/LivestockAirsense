f1a72f10cd05c54a3441e00301dda6000d644c01
ivyjanebarrios09-cloud <ivyjanebarrios09@gmail.com>
2026-06-16 03:02:06 +0800

    V

--- download.js
+++ download.js
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 
-const url = "https://fzugmubaqmfjuxdvfnur.supabase.co/storage/v1/object/public/products/1000005269-removebg-preview%20(1).png";
+const url = "https://fzugmubaqmfjuxdvfnur.supabase.co/storage/v1/object/public/products/1000005269-removebg-preview.png";
 
 async function downloadLogo() {
   try {
